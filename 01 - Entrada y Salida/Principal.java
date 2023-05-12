c46a910ce4c3a97e324c80d4ffe4f5e44d7c39fd
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-05-12 12:57:46 -0500

    Crear carpeta Actividades

---
+++ 01 - Entrada y Salida/Principal.java
@@ -0,0 +1,27 @@
+
+public class Principal{
+    public static void main(String[] args) {
+        
+        System.out.println("++------------------------------------++");
+        System.out.println("++----   SISTEMA DE FACTURACION   ----++");
+        System.out.println("++------------------------------------++");
+        System.out.println("||                                    ||");
+        System.out.println("||                      Clientes:89   ||");
+        System.out.println("||                    Vendedores:13   ||");
+        System.out.println("||                     Productos:130  ||");
+        System.out.println("||                      Facturas:30   ||");
+        System.out.println("||                                    ||");
+        System.out.println("||    1. Creacion de cliente          ||");
+        System.out.println("||    2. Creacion de Vendedor         ||");
+        System.out.println("||    3. Creacion de Factura          ||");
+        System.out.println("||    4. Ver lista de Clientes        ||");
+        System.out.println("||    5. Ver lista de Vendedores      ||");
+        System.out.println("||    6. Ver lista de Facturas        ||");
+        System.out.println("||    7. Ver detalle de Factura       ||");
+        System.out.println("||    8. Salir                        ||");
+        System.out.println("||                                    ||");
+        System.out.println("++------------------------------------++");
+        System.out.println("++------------------------------------++");
+        
+    }
+}
