e24b086fd6afc64425a46b300bbdaaab56790a69
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-05-12 14:44:01 -0500

    Carpeta variables

---
+++ 02 - Variables/Principal.java
@@ -0,0 +1,17 @@
+
+public class Principal{
+	public static void main(String[] args){
+
+		byte turno = 15;
+		short loteria_risaralda = 1247;
+		int saldo_bancario = 1500000;
+		long tarjeta_identidad = 2000000000*5;
+
+		System.out.println("turno: "+turno);
+		System.out.println("loteria_risaralda: "+loteria_risaralda);
+		System.out.println("saldo_bancario: "+saldo_bancario);
+		System.out.println("tarjeta_identidad: "+tarjeta_identidad);
+
+
+	}
+}
