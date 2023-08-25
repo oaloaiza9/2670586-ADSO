2d7c5d0c4392ec3fafd9617978f8df7ef368499f
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-08-25 13:05:38 -0500

    Agregado ejemplo de POO

---
+++ 01 - Ejemplos/06 - POO/Principal.java
@@ -0,0 +1,18 @@
+
+public class Principal{
+	public static void main(String[] args) {
+		
+		Persona persona_01 = new Persona( 108800, "Oscar", "Loaiza", 85, 185, 300.5 );
+		Persona persona_02 = new Persona( 108801, "Daniel", "Garcia", 32, 175, 110.8 );
+
+		persona_01.imprimirEstado();
+		persona_02.imprimirEstado();
+
+		persona_01.actividadFisica(80);
+		persona_02.actividadFisica(30);
+
+		persona_01.imprimirEstado();
+		persona_02.imprimirEstado();
+
+	}
+}
