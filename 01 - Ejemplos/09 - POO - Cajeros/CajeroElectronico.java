8843276f25daef56bde1c5ae594f3d69cf6b5011
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-09-08 17:20:01 -0500

    Agregando codigo ejemplo cajero y actividad 11 POO

--- 01 - Ejemplos/09 - POO - Cajeros/CajeroElectronico.java
+++ 01 - Ejemplos/09 - POO - Cajeros/CajeroElectronico.java
@@ -101,4 +101,69 @@ public class CajeroElectronico{
 		}
 	}
 
+	public void verHistorialCajero(String user, String pass){
+		if ( user.equals(usuarioAdmin) && pass.equals(claveAdmin) ) {
+			registrarTransaccion("HISTORIAL", "0000 0000 0000 0000", 0, "OK");
+
+			System.out.println("+-----------------------------------+");
+			System.out.println("|       HISTORIAL DEL CAJERO        |");
+			System.out.println("+-----------------------------------+");
+			for (int i=0; i<listaTransacciones.length; i++) {
+				if (listaTransacciones[i] != null) {
+					System.out.println("   => "+listaTransacciones[i]);
+				}
+			}
+		}else{
+			System.out.println("  ====> ACCESO DENEGADO <====");
+			registrarTransaccion("HISTORIAL", "0000 0000 0000 0000", 0, "ERROR");
+		}
+	}
+
+	public void consignarDineroaTarjeta(TarjetaDebito tarjeta, String clave, int cant_10, int cant_20, int cant_50, int cant_100){
+		int monto = (10000*cant_10) + (20000*cant_20) + (50000*cant_50) + (100000*cant_100);
+
+		// Validar la clave
+		if ( tarjeta.validarClave(clave) ) {
+			// Validar el estado de la tarjeta "ACTIVA"
+			if ( tarjeta.validarEstadoActiva() ) {
+				// El monto ingresado sea mayor a 0
+				if (monto > 0) {
+					// Validar que con el dinero ingresado no se supere el limite del cajero.
+					if ( (dineroDisponible+monto) <= capacidadTotal ) {
+						// aumentar saldo de la tarjeta
+						tarjeta.aumentarSaldo(monto, clave);
+
+						// aumentar el saldo del cajero y cantidades de billetes
+						dineroDisponible += monto;
+						this.cant_10 += cant_10;
+						this.cant_20 += cant_20;
+						this.cant_50 += cant_50;
+						this.cant_100 += cant_100;
+
+						// registar la trasaccion
+						System.out.println("  ====> REALIZADO - CONSIGNAR DINERO <====");
+						registrarTransaccion("CONSIGNACION", tarjeta.getNumero(), monto, "OK");
+					}else{
+						System.out.println("  ====> ERROR MONTO SUPERIOR - CONSIGNAR DINERO <====");
+						registrarTransaccion("CONSIGNACION", tarjeta.getNumero(), monto, "ERROR MONTO SUPERIOR");
+					}
+				}else{
+					System.out.println("  ====> ERROR MONTO INFERIOR - CONSIGNAR DINERO <====");
+					registrarTransaccion("CONSIGNACION", tarjeta.getNumero(), monto, "ERROR MONTO INFERIOR");
+				}
+			}else{
+				System.out.println("  ====> ERROR ESTADO - CONSIGNAR DINERO <====");
+				registrarTransaccion("CONSIGNACION", tarjeta.getNumero(), monto, "ERROR ESTADO");
+			}
+		}else{
+			System.out.println("  ====> ERROR PASSWORD - CONSIGNAR DINERO <====");
+			registrarTransaccion("CONSIGNACION", tarjeta.getNumero(), monto, "ERROR PASSWORD");
+		}
+		
+
+
+
+	}
+
+
 }
--- 01 - Ejemplos/09 - POO - Cajeros/TarjetaDebito.java
+++ 01 - Ejemplos/09 - POO - Cajeros/TarjetaDebito.java
@@ -1,5 +1,138 @@
+import java.util.Date;
+
 public class TarjetaDebito{
 
-	
-	
+	private String propietario;
+	private String numero;
+	private String banco;
+	private int dineroDisponible;
+	private int permitidoRetiro;
+	private String ultimasTransacciones [];
+	private String clave;
+	private String estado;
+
+	public TarjetaDebito(String propietario, String numero, String banco, int dineroDisponible, int permitidoRetiro, String clave){
+		this.propietario = propietario;
+		this.numero = numero;
+		this.banco = banco;
+		this.dineroDisponible = dineroDisponible;
+		this.permitidoRetiro = permitidoRetiro;
+		this.clave = clave;
+		this.estado = "ACTIVA";
+		this.ultimasTransacciones = new String [10];
+	}
+
+	public void registrarTransaccion(String tipo, int monto, String estado){
+		Date fecha = new Date();
+		String texto = fecha.toString()+" - "+tipo+" - "+monto+" - "+estado;
+
+		// Buscar un indice donde este vacio
+		int indice = -1;
+		for (int i=0; i<ultimasTransacciones.length; i++) {
+			if (ultimasTransacciones[i]==null) {
+				indice = i;
+				break;
+			}
+		}
+		
+		if (indice != -1) {
+			ultimasTransacciones[ indice ] = texto;
+		}else{
+			for (int i=0; i<ultimasTransacciones.length-1; i++) {
+				ultimasTransacciones[i] = ultimasTransacciones[i+1];
+			}
+			ultimasTransacciones[ ultimasTransacciones.length-1 ] = texto;
+		}
+	}
+
+	public void verHistorial(String pass){
+		if ( pass.equals(clave) ) {
+			registrarTransaccion("HISTORIAL", 0, "OK");
+			for (int i=0; i<ultimasTransacciones.length; i++) {
+				if (ultimasTransacciones[i] != null) {
+					System.out.println("  => "+ultimasTransacciones[i]);
+				}
+			}
+		}else{
+			System.out.println(" ===> ACCESO DENEGADO DESDE LA TARJETA <===");
+			registrarTransaccion("HISTORIAL", 0, "ERROR");
+		}
+	}
+
+	public boolean aumentarSaldo(int monto, String pass){
+		if ( pass.equals(clave) ) {
+			if ( monto>0 ) {
+				dineroDisponible += monto;
+				System.out.println("    ===> TRANSACCION EXITOSA EN TARJETA <===");
+				registrarTransaccion("AUMENTOSALDO", monto, "OK");
+				return true;
+			}else{
+				System.out.println("    ===> ERROR EN MONTO INGRESADO <===");
+				registrarTransaccion("AUMENTOSALDO", monto, "ERROR");
+				return false;
+			}
+		}else{
+			System.out.println("    ===> ERROR EN PASSWORD - AUMENTO DE SALDO <===");
+			registrarTransaccion("AUMENTOSALDO", monto, "ERROR");
+			return false;
+		}
+	}
+
+	public boolean disminuirSaldo(int monto, String pass){
+		if ( pass.equals(clave) ) {
+			if ( monto>0 && monto<=permitidoRetiro ) {
+				if ( monto <= dineroDisponible ) {
+					dineroDisponible -= monto;
+					System.out.println("    ===> TRANSACCION EXITOSA EN TARJETA <===");
+					registrarTransaccion("DISMINUCIONSALDO", monto, "OK");
+					return true;
+				}else{
+					System.out.println("    ===> ERROR EN MONTO INGRESADO SUPERA DINERO EN TARJETA <===");
+					registrarTransaccion("DISMINUCIONSALDO", monto, "ERROR");
+					return false;
+				}
+			}else{
+				System.out.println("    ===> ERROR EN MONTO INGRESADO - FUERA DE RANGO <===");
+				registrarTransaccion("DISMINUCIONSALDO", monto, "ERROR");
+				return false;
+			}
+		}else{
+			System.out.println("    ===> ERROR EN PASSWORD - DISMINUCION DE SALDO <===");
+			registrarTransaccion("DISMINUCIONSALDO", monto, "ERROR");
+			return false;
+		}
+	}
+
+	public int getSaldo(String pass){
+		if ( pass.equals(clave) ) {
+			registrarTransaccion("CONSULTASALDO", 0, "OK");
+			return dineroDisponible;
+		}else{
+			registrarTransaccion("CONSULTASALDO", 0, "ERROR");
+			return -1;
+		}
+	}
+
+	public boolean validarClave(String pass){
+		if ( pass.equals(clave) ) {
+			registrarTransaccion("VALIDARCLAVEINTERNA", 0, "OK");
+			return true;
+		}else{
+			registrarTransaccion("VALIDARCLAVEINTERNA", 0, "ERROR");
+			return false;
+		}
+	}
+
+	public boolean validarEstadoActiva(){
+		if ( estado.equals("ACTIVA") ) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public String getNumero(){
+		return numero;
+	}
+
 }
