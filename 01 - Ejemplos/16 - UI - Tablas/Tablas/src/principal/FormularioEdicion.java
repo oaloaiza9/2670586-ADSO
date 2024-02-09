68d50283a8538b1a25d89b3304dd26566b54108b
Oscar Andres Loaiza <oaloaiza9@misena.edu.co>
2024-02-09 16:03:17 -0500

    Formularios

--- 01 - Ejemplos/16 - UI - Tablas/Tablas/src/principal/FormularioEdicion.java
+++ 01 - Ejemplos/16 - UI - Tablas/Tablas/src/principal/FormularioEdicion.java
@@ -1,116 +1,17 @@
 package principal;
 
-import java.awt.Color;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import utils.Persona;
-import java.util.Vector;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.SwingConstants;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import utils.ButtonEditor;
-import utils.ButtonRenderer;
+public class FormularioEdicion extends javax.swing.JFrame {
 
-public class TablaBotones extends javax.swing.JFrame {
-    
-    DefaultTableModel modelo; 
-    Persona listaPersonas[];
-    
-    public TablaBotones() {
-        listaPersonas = new Persona[5];
-        listaPersonas[0] = new Persona("108800", "Oscar", "Loaiza", "3333330", "oscar@mail.com");
-        listaPersonas[1] = new Persona("108801", "Daniel", "Garcia", "3333331", "daniel@mail.com");
-        listaPersonas[2] = new Persona("108802", "Juan", "Lopez", "3333332", "juan@mail.com");
-        listaPersonas[3] = new Persona("108803", "Maria", "Perez", "3333333", "maria@mail.com");
-        listaPersonas[4] = new Persona("108804", "Carlos", "Toro", "3333334", "carlos@mail.com");
-        
+    public FormularioEdicion() {
         initComponents();
-        initAternComponents();
-        imprimirPersonas();
+        initAlternComponents();
     }
     
-    public void initAternComponents(){
-        setTitle("Tabla Basica");
-        setLocationRelativeTo(null);
+    public void initAlternComponents(){
+        setTitle("Formulario de Edicion");
         setVisible(true);
+        setLocationRelativeTo(null);
         setIconImage( getToolkit().createImage( ClassLoader.getSystemResource("imagenes/icono_registro.png") ) );
-        
-        modelo = (DefaultTableModel) tablaDatos.getModel();
-        
-        tablaDatos.getColumnModel().getColumn(5).setCellEditor(new ButtonEditor(new JCheckBox()));
-        tablaDatos.getColumnModel().getColumn(5).setCellRenderer(new ButtonRenderer());
-        
-        tablaDatos.getColumnModel().getColumn(6).setCellEditor(new ButtonEditor(new JCheckBox()));
-        tablaDatos.getColumnModel().getColumn(6).setCellRenderer(new ButtonRenderer());
-        
-        // Tamaño de Columnas
-        tablaDatos.getColumnModel().getColumn(0).setPreferredWidth(50);
-        tablaDatos.getColumnModel().getColumn(1).setPreferredWidth(150);
-        tablaDatos.getColumnModel().getColumn(2).setPreferredWidth(150);
-        tablaDatos.getColumnModel().getColumn(3).setPreferredWidth(50);
-        tablaDatos.getColumnModel().getColumn(4).setPreferredWidth(150);
-        tablaDatos.getColumnModel().getColumn(5).setPreferredWidth(10);
-        tablaDatos.getColumnModel().getColumn(6).setPreferredWidth(10);
-        
-        // Ajuste del Orden y Ancho de Columnas
-        tablaDatos.getTableHeader().setReorderingAllowed(false);
-        tablaDatos.getTableHeader().setResizingAllowed(false);
-        
-        // Centrar contenido de columnas
-        DefaultTableCellRenderer centerRender = new DefaultTableCellRenderer();
-        centerRender.setHorizontalAlignment(SwingConstants.CENTER);
-        tablaDatos.getColumnModel().getColumn(0).setCellRenderer(centerRender);
-        tablaDatos.getColumnModel().getColumn(3).setCellRenderer(centerRender);
-        
-        // Alto de las filas
-        tablaDatos.setRowHeight(25);
-    }
-    
-    public void imprimirPersonas(){
-        modelo.setRowCount(0);
-        for (int i=0;i<listaPersonas.length && listaPersonas[i]!=null; i++) {
-            String documento = listaPersonas[i].getDocumento();
-            String nombres = listaPersonas[i].getNombres();
-            String apellidos = listaPersonas[i].getApellidos();
-            String telefono = listaPersonas[i].getTelefono();
-            String correo = listaPersonas[i].getCorreo();
-            
-            JButton btnEditar = new JButton();
-            btnEditar.setBackground(Color.white);
-            Image icono_editar = getToolkit().createImage( ClassLoader.getSystemResource("imagenes/icono_editar.png") );
-            icono_editar = icono_editar.getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-            btnEditar.setIcon( new ImageIcon(icono_editar) );
-            
-            JButton btnEliminar = new JButton();
-            btnEliminar.setBackground(Color.white);
-            Image icono_eliminar = getToolkit().createImage( ClassLoader.getSystemResource("imagenes/icono_eliminar.png") );
-            icono_eliminar = icono_eliminar.getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-            btnEliminar.setIcon( new ImageIcon(icono_eliminar) );
-            
-            Object dato[] = new Object[]{ documento, nombres, apellidos, telefono, correo, btnEditar, btnEliminar };
-            modelo.addRow(dato);
-            
-            btnEditar.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("Nombres: " + nombres);
-                    FormularioEdicion ventana = new FormularioEdicion();
-                }
-            });
-            
-            btnEliminar.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    EliminarPersona ventana = new EliminarPersona();
-                }
-            });
-            
-            
-        }
     }
     
     @SuppressWarnings("unchecked")
@@ -131,18 +32,15 @@ public class TablaBotones extends javax.swing.JFrame {
         etqCorreo = new javax.swing.JLabel();
         campoCorreo = new javax.swing.JTextField();
         btnAgregar = new javax.swing.JButton();
-        contenedorDatos = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        tablaDatos = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         contenedorTitulo.setBackground(new java.awt.Color(0, 51, 153));
 
-        etqTitulo.setFont(new java.awt.Font("Arial", 1, 36)); // NOI18N
+        etqTitulo.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         etqTitulo.setForeground(new java.awt.Color(255, 255, 255));
         etqTitulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        etqTitulo.setText("Tabla con Botones");
+        etqTitulo.setText("EDITAR PERSONA");
 
         javax.swing.GroupLayout contenedorTituloLayout = new javax.swing.GroupLayout(contenedorTitulo);
         contenedorTitulo.setLayout(contenedorTituloLayout);
@@ -157,7 +55,7 @@ public class TablaBotones extends javax.swing.JFrame {
             contenedorTituloLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(contenedorTituloLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(etqTitulo, javax.swing.GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
+                .addComponent(etqTitulo, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -191,7 +89,7 @@ public class TablaBotones extends javax.swing.JFrame {
         btnAgregar.setBackground(new java.awt.Color(0, 153, 0));
         btnAgregar.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         btnAgregar.setForeground(new java.awt.Color(255, 255, 255));
-        btnAgregar.setText("AGREGAR");
+        btnAgregar.setText("EDITAR");
         btnAgregar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnAgregarActionPerformed(evt);
@@ -214,7 +112,7 @@ public class TablaBotones extends javax.swing.JFrame {
                             .addComponent(etqCorreo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(campoCorreo, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnAgregar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(btnAgregar, javax.swing.GroupLayout.DEFAULT_SIZE, 192, Short.MAX_VALUE))
                     .addGroup(contenedorFormularioLayout.createSequentialGroup()
                         .addGroup(contenedorFormularioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(etqDocumento, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -258,53 +156,7 @@ public class TablaBotones extends javax.swing.JFrame {
                         .addGroup(contenedorFormularioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(campoCorreo, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(btnAgregar, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addGap(25, 25, 25))
-        );
-
-        contenedorDatos.setBackground(new java.awt.Color(153, 255, 255));
-
-        jScrollPane1.setBackground(new java.awt.Color(255, 0, 0));
-
-        tablaDatos.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-
-            },
-            new String [] {
-                "Documento", "Nombres", "Apellidos", "Telefono", "Correo Elec.", "", ""
-            }
-        ) {
-            Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class, java.lang.Object.class
-            };
-            boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, true, true
-            };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
-            }
-        });
-        jScrollPane1.setViewportView(tablaDatos);
-
-        javax.swing.GroupLayout contenedorDatosLayout = new javax.swing.GroupLayout(contenedorDatos);
-        contenedorDatos.setLayout(contenedorDatosLayout);
-        contenedorDatosLayout.setHorizontalGroup(
-            contenedorDatosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(contenedorDatosLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1)
-                .addContainerGap())
-        );
-        contenedorDatosLayout.setVerticalGroup(
-            contenedorDatosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(contenedorDatosLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 267, Short.MAX_VALUE)
-                .addContainerGap())
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -313,16 +165,13 @@ public class TablaBotones extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(contenedorTitulo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(contenedorFormulario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(contenedorDatos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(contenedorTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(contenedorFormulario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(contenedorDatos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(contenedorFormulario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
@@ -334,50 +183,13 @@ public class TablaBotones extends javax.swing.JFrame {
         String apellidos = campoApellidos.getText();
         String telefono = campoTelefono.getText();
         String correo = campoCorreo.getText();
-        
+
         if( documento.equals("") || nombres.equals("") || apellidos.equals("") || telefono.equals("") || correo.equals("") ){
-            Alerta ventana = new Alerta("Todos los campos son Obligatorios.");  
+            Alerta ventana = new Alerta("Todos los campos son Obligatorios.");
         }else{
             
-            // Buscar posicon vacia
-            int posicion = -1;
-            for (int i=0;i<listaPersonas.length;i++) {
-                if (listaPersonas[i]==null) {
-                    posicion = i;
-                    break;
-                }
-            }
-            
-            boolean valido = true;
-            for (int i=0;i<listaPersonas.length && listaPersonas[i]!=null; i++) {
-                if (listaPersonas[i].getDocumento().equalsIgnoreCase(documento) || listaPersonas[i].getCorreo().equalsIgnoreCase(correo) ) {
-                    valido = false;
-                    break;
-                }
-            }
-            
-            // Crear objeto en posicion vacia
-            if (posicion!=-1 && valido) {
-                listaPersonas[posicion] = new Persona(documento, nombres, apellidos, telefono, correo);
-                
-                Object data[] = new Object[]{ documento, nombres, apellidos, telefono, correo };
-                modelo.addRow(data);
-
-                campoDocumento.setText("");
-                campoNombres.setText("");
-                campoApellidos.setText("");
-                campoTelefono.setText("");
-                campoCorreo.setText("");
-                campoDocumento.requestFocus();
-            }else{
-                if (posicion==-1) {
-                    Alerta ventana = new Alerta("No se dispone de memoria.");
-                }else if(!valido){
-                    Alerta ventana = new Alerta("El documeto o el correo se repiten.");
-                }
-            }
         }
-        
+
     }//GEN-LAST:event_btnAgregarActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -387,7 +199,6 @@ public class TablaBotones extends javax.swing.JFrame {
     private javax.swing.JTextField campoDocumento;
     private javax.swing.JTextField campoNombres;
     private javax.swing.JTextField campoTelefono;
-    private javax.swing.JPanel contenedorDatos;
     private javax.swing.JPanel contenedorFormulario;
     private javax.swing.JPanel contenedorTitulo;
     private javax.swing.JLabel etqApellidos;
@@ -396,7 +207,5 @@ public class TablaBotones extends javax.swing.JFrame {
     private javax.swing.JLabel etqNombres;
     private javax.swing.JLabel etqTelefono;
     private javax.swing.JLabel etqTitulo;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable tablaDatos;
     // End of variables declaration//GEN-END:variables
 }
